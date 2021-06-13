638fb237613a487e5ed403f7a9e9fcd0d586a4f7
GOWTHAMAN R <61967645+Gowthaman1401@users.noreply.github.com>
2021-06-13 22:10:58 +0530

    setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='manpasswd',
     packages=setuptools.find_packages(),
-    version='2.4.4',
+    version='1.0.0',
     license='MIT',
     description='Password Manager using PostgreSQL',
     author='Gowthaman',
