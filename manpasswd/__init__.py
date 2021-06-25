94409a662b089516b13f94df5c63725b1551c208
GOWTHAMAN R <61967645+Gowthaman1401@users.noreply.github.com>
2021-06-25 20:32:41 +0530

    Delete __init__.py

--- manpasswd/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Password Manager using PostgreSQL"""
-
-__version__ = '1.0.0'
-
-from .manpasswd import Manpasswd
-from .Menu import Data, Menu, Key
-from .Psql import Psql, tabulate_retrieved
-from .Key import MasterKey
